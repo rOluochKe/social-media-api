d234ede980fbf90cac959b2de7dbc7c504e480ea
Raymond Oluoch <rasholuoch@gmail.com>
2021-03-30 03:16:40 +0300

    adding user follow and unfollow

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,18 @@ const {
   getUser, 
   updateUser, 
   deleteUser,
-  userPhoto } = require('../controllers/user');
+  userPhoto,
+  addFollowing,
+  addFollower,
+  removeFollowing,
+  removeFollower,
+  findPeople } = require('../controllers/user');
 const { requireSignin } = require("../controllers/auth");
 
 const router = express.Router();
+
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
+router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
 
 router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
@@ -16,6 +24,8 @@ router.put('/user/:userId', requireSignin, updateUser);
 router.delete('/user/:userId', requireSignin, deleteUser);
 // photo
 router.get("/user/photo/:userId", userPhoto);
+// who to follow
+router.get("/user/findpeople/:userId", requireSignin, findPeople);
 
 // any route containing :userId, our app will first execute userByID()
 router.param('userId', userById);
