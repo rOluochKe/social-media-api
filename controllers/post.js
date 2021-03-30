028cedb9dcc6c34d822ea329b3771aba8e379990
Raymond Oluoch <rasholuoch@gmail.com>
2021-03-30 22:48:50 +0300

    users posts operations

--- controllers/post.js
+++ controllers/post.js
@@ -20,9 +20,10 @@ exports.postById = (req, res, next, id) => {
 exports.getPosts = (req, res) => {
   const posts = Post.find()
     .populate("postedBy", "_id name")
-    .select("_id title body")
+    .select("_id title body created")
+    .sort({ created: -1 })
     .then((posts) => {
-      res.json({ posts });
+      res.json(posts);
     })
     .catch((err) => console.log(err));
 };
@@ -85,6 +86,20 @@ exports.isPoster = (req, res, next) => {
   next();
 };
 
+// exports.updatePost = (req, res, next) => {
+//     let post = req.post;
+//     post = _.extend(post, req.body);
+//     post.updated = Date.now();
+//     post.save(err => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: err
+//             });
+//         }
+//         res.json(post);
+//     });
+// };
+
 exports.updatePost = (req, res, next) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
@@ -129,4 +144,13 @@ exports.deletePost = (req, res) => {
       message: "Post deleted successfully",
     });
   });
+};
+
+exports.photo = (req, res, next) => {
+  res.set("Content-Type", req.post.photo.contentType);
+  return res.send(req.post.photo.data);
+};
+
+exports.singlePost = (req, res) => {
+  return res.json(req.post);
 };
--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,9 @@ const {
   postById,
   isPoster,
   updatePost,
-  deletePost } = require('../controllers/post');
+  deletePost,
+  photo,
+  singlePost } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const { createPostValidator } = require('../validator');
@@ -17,8 +19,11 @@ const router = express.Router();
 router.get('/posts', getPosts);
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
 router.get('/posts/by/:userId', requireSignin, postsByUser);
+router.get('/post/:postId', singlePost);
 router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+// photo
+router.get('/post/photo/:postId', photo);
 
 // any route containing :userId, our app will first execute userById()
 router.param('userId', userById);
